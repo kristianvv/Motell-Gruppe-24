06322abdf3b3783a5a1b90473a8e25536775c555
felixlhh <felixlh@uia.no>
2024-10-07 13:29:06 +0200

    opprettet mappestruktur og lagt inn dbconnection fil

---
+++ includes/dbconnect.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'hotell');
+
+$dkn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+
+try {
+   $pdo = new PDO($dkn, DB_USER, DB_PASS);
+}
+    catch (PDOException $e) {
+        echo "Feil ved tilkobling til databasen: " . $e-> getMessage();
+    }
+
+    register_shutdown_function(function() use (&$pdo) {
+        $pdo = null;
+    })
+
+?>
