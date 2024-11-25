73bec43f2194ec6d1306d094824e9fda086b4609
felixlhh <felixlh@uia.no>
2024-11-25 13:01:45 +0100

    endret sql

--- motell.sql
+++ motell.sql
@@ -3,11 +3,11 @@ CREATE DATABASE IF NOT EXISTS `motell`;
 USE `motell`;
 
 CREATE TABLE IF NOT EXISTS `User` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) NOT NULL UNIQUE,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(10) DEFAULT NULL
+  `userID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(100) DEFAULT NULL,
+  `email` VARCHAR(100) NOT NULL UNIQUE,
+  `password` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(10) DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `Rooms` (
@@ -22,6 +22,11 @@ CREATE TABLE IF NOT EXISTS `Booking` (
   `checkInDate` DATE NOT NULL,
   `checkOutDate` DATE NOT NULL,
   `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (`roomID`) REFERENCES `Rooms`(`roomID`)
+  `roomID` INT NOT NULL,
+  `userID` INT NOT NULL,
+  FOREIGN KEY (`roomID`) REFERENCES `Rooms`(`roomID`),
   FOREIGN KEY (`userID`) REFERENCES `User`(`userID`)
 );
+
+
+CREATE VIEW IF NOT EXISTS `RoomAvailability` AS SELECT
